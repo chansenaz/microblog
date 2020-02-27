1180a61a14d9e05c8015b34e434379eda0c32913
Chris Hansen <chansen2@asu.edu>
2020-02-26 19:07:33 -0700

    added profile page and avatars

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,10 @@
 from flask import render_template, flash, redirect, url_for, request
 from app import app, db
-from app.forms import LoginForm, RegistrationForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
 from werkzeug.urls import url_parse
+from datetime import datetime
 
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -60,3 +61,49 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
+
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit Profile', form=form)
+
+
+# The @before_request decorator from Flask register the decorated function to
+# be executed right before the view function. This is extremely useful because
+# now I can insert code that I want to execute before any view function in
+# the application, and I can have it
+# in a single place.
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+
+        # If you are wondering why there is no db.session.add() before the
+        # commit, consider that when you reference current_user, Flask-Login
+        # will invoke the user loader callback function, which will run a
+        # database query that will put the target user in the database session.
+        # So you can add the user again in this function, but it is not
+        # necessary because it is already there.
+        db.session.commit()
